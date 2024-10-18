0d7e0409bb6f78a1ff4e32419a41878ebed318f1
Luis Perez <ljpm9401@gmail.com>
2024-10-17 19:37:27 -0500

    Avances

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -8,6 +8,107 @@
       h1 Creación y configuración de un nuevo documento en Adobe Illustrator
     
     .row
+      .col-lg-5.mb-4.mb-lg-0(data-aos="fade-right")
+        p Cuando se va a crear un documento en Illustrator, lo primero que se debe considerar es el propósito del proyecto a realizar, debido que esto dicta el tipo de documento a preparar y sus características. Si el objetivo del proyecto es la web, seguro ha de requerir un modo de color diferente, como si fuera uno destinado para impresión offset, lo mismo ocurre con el tamaño y las unidades de medida.
+        .p-3.mt-4(style="background-color:#FFEACC;")
+          .row
+            .col-auto.d-none.d-md-block(style="place-self:center;" data-aos="zoom-in-left" data-aos-duration="650")
+              figure
+                img(src="@/assets/curso/tema1/1.svg")
+            .col(style="place-self:center;" data-aos="fade-up" data-aos-duration="400")
+              p.mb-0 Para configurar el nuevo documento se va al menú #[strong Archivo > Nuevo], esto deberá abrir una ventana con el título #[strong Nuevo documento].
+        p.mt-4.mb-0 El cuadro de diálogo Nuevo documento ofrece una variedad de opciones para iniciar sus proyectos en Adobe Illustrator. Entre las principales funciones se incluyen:
+      .col-lg-7(data-aos="fade-left")
+        figure.m-auto.col-12.col-md-9.col-lg-12
+          img(src="@/assets/curso/tema1/2.png")
+
+    .row.mt-5
+      .col-lg-3.d-none.d-lg-block(data-aos="fade-right")
+        figure 
+          img(src="@/assets/curso/tema1/3.png")
+      .col-lg-9(data-aos="zoom-in")
+        AcordionA(tipo="a" clase-tarjeta="tarjeta")
+          .row.col-xl-11.m-auto(titulo="Crear documentos a partir de plantillas")
+            p Selecciona plantillas de Adobe Stock organizadas en diversas categorías como reciente, guardado, móvil, web, impresión, película y video, arte e ilustración, y plantillas gratuitas.
+          .row.col-xl-11.m-auto(titulo="Buscar y utilizar plantillas adicionales")
+            p Se encarga de analizar la estructura de los datos y que se cumpla con los requisitos del negocio, modelo de negocio o problema. Las actividades a desarrollar, son:
+            ul.lista-ul.mb-0.ps-3
+              li.d-flex
+                i.fas.fa-bezier-curve(style="color:#FF7650;")
+                span Mapear diagrama entidad relación a un modelo relacional.
+              li.d-flex
+                i.fas.fa-bezier-curve(style="color:#FF7650;")
+                span Análisis de restricciones de integridad.
+              li.d-flex
+                i.fas.fa-bezier-curve(style="color:#FF7650;")
+                span Validación de requisitos funcionales.
+          .row.col-xl-11.m-auto(titulo="Acceso rápido a archivos y plantillas recientes")
+            p En la pestaña de componentes recientes, puede acceder rápidamente a archivos, plantillas y otros elementos utilizados recientemente.
+          .row.col-xl-11.m-auto(titulo="Acceso a plantillas guardadas")
+            p Encuentra rápidamente las plantillas que ha descargado de Adobe Stock en la pestaña Guardados.
+          .row.col-xl-11.m-auto(titulo="Crear documentos con ajustes preestablecidos")
+            p Usa los ajustes preestablecidos de documento en blanco, disponibles para diversas categorías y dispositivos, y ajusta su configuración, según sus necesidades, antes de abrir el documento.
+    
+    Separador
+
+    .titulo-segundo(data-aos="flip-up")
+      h2#t_1_1 1.1 Crear y guardar ajustes prestablecidos de documentos personalizados
+    
+    p(data-aos="fade-right") Antes de abrir un documento con un ajuste preestablecido, se puede personalizar utilizando las opciones del panel derecho.
+
+    .cont_1_1.mt-4
+      .row
+        .col-lg-5.mb-4.mb-lg-0(data-aos="fade-right")
+          .titulo-sexto.color-acento-contenido(data-aos="zoom-in-down")
+            h5 Figura 1.
+            span Panel derecho
+          .cont_1_2.p-4.pt-xl-5.pb-xl-5
+            figure(data-aos="zoom-in-up")
+              img(src="@/assets/curso/tema1/5.svg" alt="Imagen que presenta las opciones del panel derecho.")
+        .col-lg-7(data-aos="fade-left")
+          p.fw-bold.mb-0 A. Nombre del documento
+          p Asigne un nombre al nuevo documento.
+          p.fw-bold.mb-0 B. Configuraciones del ajuste preestablecido
+          p Personalice las siguientes opciones para el ajuste preestablecido seleccionado:
+          ul.lista-ul.mb-3.ps-3
+            li.d-flex.mb-1
+              i.fas.fa-bezier-curve(style="color:#FF7650;")
+              span #[strong Anchura y altura:] defina el tamaño de la mesa de trabajo y seleccione la unidad de medida en el menú desplegable.
+            li.d-flex.mb-1
+              i.fas.fa-bezier-curve(style="color:#FF7650;")
+              span #[strong Orientación:] elija la orientación del documento, ya sea horizontal o vertical.
+            li.d-flex.mb-1
+              i.fas.fa-bezier-curve(style="color:#FF7650;")
+              span #[strong Mesas de trabajo:] especifique el número de mesas de trabajo que desea en el documento.
+            li.d-flex.mb-1
+              i.fas.fa-bezier-curve(style="color:#FF7650;")
+              span #[strong Sangrado:] establezca el valor de sangrado para cada lado de la mesa de trabajo. Si necesita diferentes valores para cada lado, seleccione la opción correspondiente.
+            li.d-flex.mb-1
+              i.fas.fa-bezier-curve(style="color:#FF7650;")
+              span #[strong Modo de color:] elija el modo de color del documento, ya sea RGB para trabajos digitales o CMYK para proyectos impresos.
+            li.d-flex.mb-0
+              i.fas.fa-bezier-curve(style="color:#FF7650;")
+              span #[strong Efectos de rasterizado:] defina la resolución para los efectos de rasterizado en el documento.
+          p.fw-bold.mb-0 C. Modo de previsualización
+          p Configure el modo de previsualización predeterminado del documento:
+          ul.lista-ul.mb-3.ps-3
+            li.d-flex.mb-1
+              i.fas.fa-bezier-curve(style="color:#FF7650;")
+              span #[strong Por defecto:] muestra la ilustración en vista vectorial a todo color, conservando el suavizado en las curvas al acercar o alejar.
+            li.d-flex.mb-1
+              i.fas.fa-bezier-curve(style="color:#FF7650;")
+              span #[strong Píxel:] presenta una vista previa simulada con apariencia rasterizada, sin alterar el contenido vectorial real.
+            li.d-flex.mb-0
+              i.fas.fa-bezier-curve(style="color:#FF7650;")
+              span #[strong Sobreimpresión:] ofrece una "previsualización de tintas", simulando cómo se verán las fusiones, transparencias y sobreimpresiones en una salida con separación de color.
+          p.fw-bold.mb-0 D. Más ajustes
+          p.mb-0 Si necesita ajustar aún más el preestablecido, seleccione la opción Más ajustes para acceder a configuraciones adicionales.
+
+
+
+
+
+
       
 </template>
 
--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -5,7 +5,128 @@
     .titulo-principal.color-acento-contenido(data-aos="flip-up")
       .titulo-principal__numero
         span 2
-      h1 Titulo de primer nivel
+      h1 Importación de imágenes a Adobe Illustrator
+    
+    .bloque-texto-g.color-secundario.p-3.p-sm-4.p-md-5.mt-4(data-aos="flip-up")
+      .bloque-texto-g__img(
+        :style="{'background-image': `url(${require('@/assets/curso/tema2/1.svg')})`}"
+      )
+      .bloque-texto-g__texto.p-4
+        p.mb-0 Adobe Illustrator brinda la opción de importar imágenes vectoriales o de mapas de bits, lo que da la ventaja de comenzar con un gran avance cualquier proyecto de diseño. Este software identifica los formatos de imágenes más utilizados, además, tiene especial facilidad para importar y exportar documentos con formatos específicos de programas de la misma casa matriz. Esto hace que sea fácil importar y exportar, desde la ventana de diálogo o simplemente copiando y pegando.
+    
+    Separador
+
+    .titulo-segundo(data-aos="flip-up")
+      h2#t_2_1 2.1 Colocación (importación) de archivos
+    
+    .row
+      .col-lg-7(data-aos="fade-right")
+        p Para importar un archivo, el método principal es #[strong Colocar (Shift + Ctrl + P o Archivo > Colocar…)], aunque es posible, en muchos casos, simplemente copiar imágenes o archivos de otros programas y pegarlos en el proyecto de Adobe Illustrator. Solamente #[strong Colocar] proporciona el máximo nivel de compatibilidad con formatos externos, las opciones de colocación y el color.
+        .p-3.mt-4(style="background-color:#FFEACC;")
+          .row
+            .col-auto.d-none.d-md-block(style="place-self:center;")
+              figure
+                img(src="@/assets/curso/tema2/2.svg")
+            .col(style="place-self:center;")
+              p.mb-0 El cuadro de diálogo de importación de Adobe Illustrator es una ventana estándar de abrir archivo del sistema operativo que se use, sin embargo, ofrece unas cuantas casillas de selección que permiten configurar la importación.
+        p.mt-4.mb-0 Al momento de importar se selecciona la casilla #[strong Enlazar] para crear un enlace al archivo, o se deselecciona para incrustar la imagen en el documento. Es además, altamente recomendable activar la casilla #[strong Mostrar opciones de importación], estas opciones cambian de acuerdo con el tipo de archivo que se está importando.
+      .col-lg-5.d-none.d-lg-block(data-aos="fade-left")
+        figure
+          img(src="@/assets/curso/tema2/3.svg")
+    
+    .col-xl-10.m-auto.mt-5
+      .cont_2_1.p-4
+        .col-xl-8.m-auto
+          figure(data-aos="zoom-in-up")
+            img(src="@/assets/curso/tema2/4.svg")
+    
+    .row.mt-5
+      .col-lg-4.d-none.d-lg-block(data-aos="fade-right")
+        figure
+          img(src="@/assets/curso/tema2/5.svg")
+      .col-lg-8(data-aos="fade-up")
+        p Cuando coloque un gráfico, verá una versión con la resolución de pantalla del archivo en el diseño, de manera que podrá verlo y colocarlo, aunque el archivo del gráfico real puede estar enlazado o incrustado.
+        .p-3.mt-4(style="background-color:#EAEBFD;")
+          .row
+            .col(style="place-self:center;")
+              p.mb-0 Una imagen enlazada está presente, pero enlaza a un archivo exterior al documento, por lo que el peso de esta no afecta directamente el peso total del documento. Las imágenes enlazadas pueden modificarse, usando efectos y herramientas de transformación, sin embargo, no pueden modificarse por partes individuales y los cambios que se efectúen por fuera del ilustrador se verán reflejados en la imagen que aparece en el documento.
+            .col-auto.d-none.d-md-block(style="place-self:center;")
+              figure
+                img(src="@/assets/curso/tema2/6.svg")
+        p.mt-4.mb-0 Las imágenes incrustadas se integran en el documento completamente con su mayor resolución, lo que afecta en gran medida el peso general del archivo del proyecto.
+    
+    Separador
+
+    .titulo-segundo(data-aos="flip-up")
+      h2#t_2_2 2.2 Calco de imagen
+    
+    p(data-aos="fade-left") Es posible vectorizar una imagen de mapa de bits si por ejemplo se desea que un diseño nuevo se base en una imagen existente. Para eso se usa el comando Calco de imagen que aparece en la barra de control.
+
+    .cont_2_2.mt-5
+      .col-xl-10.m-auto
+        span.h5.p-1.ps-2.pe-2(style="background-color:#FF7650;") Especificar las opciones de calco
+        p.mt-3(data-aos="fade-up") Cuando se selecciona la imagen, las opciones del panel #[strong Calco de imagen] se activan. En la parte superior del panel, se encuentran las opciones básicas de calco. Para acceder a opciones adicionales, haga clic en el triángulo junto a la etiqueta Avanzado, lo que desplegará configuraciones más detalladas.
+
+        .cont_2_1.p-4
+          .col-xl-8.m-auto
+            figure(data-aos="zoom-in-up")
+              img.d-none.d-md-block(src="@/assets/curso/tema2/8.png")
+              img.d-md-none(src="@/assets/curso/tema2/9.png")
+        
+        .mt-5
+          span.h5.p-1.ps-2.pe-2(style="background-color:#FF7650;") Ajuste preestablecido
+          p.mt-3(data-aos="fade-up") El panel #[strong Calco de imagen] permite seleccionar un ajuste preestablecido para la imagen. Los íconos representan ajustes preestablecidos diseñados para los flujos de trabajo más comunes.
+
+        .cont_2_1.p-4.ps-xl-5.pe-xl-5.mt-4
+          figure(data-aos="zoom-in-up")
+            img.d-none.d-md-block(src="@/assets/curso/tema2/10.png")
+            img.d-md-none(src="@/assets/curso/tema2/11.png")
+    
+    .row.mt-5.justify-content-center
+      .col-lg.mb-4.mb-lg-0.col-md-6(data-aos="zoom-in")
+        .h-100.p-3.cont_2_3
+          figure.mb-3
+            img(src="@/assets/curso/tema2/12.svg")
+          .m-auto.mb-3.text-center
+            p.fw-bold.mb-0.p-1.ps-2.pe-2 Color automático
+          p.mb-0 Crea una imagen posterizada a partir de una foto o una ilustración.
+      .col-lg.mb-4.mb-lg-0.col-md-6(data-aos="zoom-in")
+        .h-100.p-3.cont_2_3
+          figure.mb-3
+            img(src="@/assets/curso/tema2/13.svg")
+          .m-auto.mb-3.text-center
+            p.fw-bold.mb-0.p-1.ps-2.pe-2 Color alto
+          p.mb-0 Crea ilustraciones fotorrealistas de alta fidelidad.
+      .col-lg.mb-4.mb-lg-0.col-md-6(data-aos="zoom-in")
+        .h-100.p-3.cont_2_3
+          figure.mb-3
+            img(src="@/assets/curso/tema2/14.svg")
+          .m-auto.mb-3.text-center
+            p.fw-bold.mb-0.p-1.ps-2.pe-2 Color bajo
+          p.mb-0 Crea ilustraciones fotorrealistas simples.
+      .col-lg.mb-4.mb-lg-0.col-md-6(data-aos="zoom-in")
+        .h-100.p-3.cont_2_3
+          figure.mb-3
+            img(src="@/assets/curso/tema2/15.svg")
+          .m-auto.mb-3.text-center
+            p.fw-bold.mb-0.p-1.ps-2.pe-2 Escala de grises
+          p.mb-0 Calca la ilustración en tonos grises.
+      .col-lg.mb-4.mb-lg-0.col-md-6(data-aos="zoom-in")
+        .h-100.p-3.cont_2_3
+          figure.mb-3
+            img(src="@/assets/curso/tema2/16.svg")
+          .m-auto.mb-3.text-center
+            p.fw-bold.mb-0.p-1.ps-2.pe-2 Blanco y negro
+          p.mb-0 Simplifica la imagen en una ilustración en blanco y negro.
+      .col-lg.mb-4.mb-lg-0.col-md-6(data-aos="zoom-in")
+        .h-100.p-3.cont_2_3
+          figure.mb-3
+            img(src="@/assets/curso/tema2/17.svg")
+          .m-auto.mb-3.text-center
+            p.fw-bold.mb-0.p-1.ps-2.pe-2 Contornear
+          p.mb-0 Reduce la imagen a contornos en negro
+
+
 
 </template>
 
